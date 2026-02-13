81b2613a5f45ec310e403561d66ca87ab6ad16a9
deepuchary03 <deepuchary03@gmail.com>
2026-02-13 14:47:20 +0530

    update

--- auth/college/students/views.py
+++ auth/college/students/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from .models import StudentProfiles
 
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirmpassword']
+        if password == confirm_password:
+            user = User.objects.create_user(username=username, password=password)
+            StudentProfiles.objects.create(user=user)
+            return redirect('login')
+    return render(request, 'register.html')
+
+
+
